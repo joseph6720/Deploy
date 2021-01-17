06ed6794a096a9afee5b10dcf60d59823fb4d400
joseph6720 <joseph.marshal.ng99@gmail.com>
2021-01-17 17:49:17 +0300

    simplifying the app

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const courses = require('./routes/courses');
-const home = require('./routes/home')
 app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/courses', courses);
-app.use('/', home);
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello World Joseph Amazon');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
