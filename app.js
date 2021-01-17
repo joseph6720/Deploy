fed54302e2a7c77c07340bd4258e96ead62b04bc
joseph6720 <joseph.marshal.ng99@gmail.com>
2021-01-17 18:23:16 +0300

    dynamic webpage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 app = express();
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.status(200).send('Hello World Joseph Amazon');
+	res.render('index', {
+		title   : 'My Express App',
+		message : 'Hello Pug dynamic web'
+	});
 });
 
 const port = process.env.PORT || 3000;
